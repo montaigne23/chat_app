c0cd5d9ce616c9ac2c28b80ecce7d8fb51f1358f
montaigne23 <www.landau.com@gmail.com>
2023-04-10 19:52:19 +0100

    First commit App Chat v1.0.0

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+// module.exports = {
+//  url : "mongodb://localhost:27017/mongochat"
+//   };
+module.exports = {
+ url : "mongodb+srv://mongochat:WWVwoKHkaas2pULu@cluster0.nzusr.mongodb.net/mongochat?retryWrites=true&w=majority"
+  };
